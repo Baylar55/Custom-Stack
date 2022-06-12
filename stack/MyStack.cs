d008214bf120ab9ff8b9e2ecb7a58f95572bd71a
Beyler55 <baylarss@code.edu.az>
2022-06-12 23:40:48 +0400

    custom stack

---
+++ stack/MyStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    class MyStack<T> : IEnumerable
+    {
+
+        public int Count { get; set; }
+        private int Capacity { get; set; }
+        private T[] arr;
+        
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = Count-1; i >= 0; i--)
+            {
+                yield return arr[i];
+            }
+        }
+        
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private int index = -1;
+        
+
+        public MyStack(int capacity)
+        {
+            Capacity = capacity;
+            arr = new T[capacity];
+        }
+        
+
+        public void Push(T element)
+        {
+            if (Count < Capacity)
+            {
+                Array.Resize(ref arr, Capacity);
+                index++;
+                arr[index] = element;
+                
+            }
+            else if (Count > Capacity || Count == Capacity)
+            {
+                Capacity = Capacity * 2;
+                Array.Resize(ref arr, Capacity);
+                index++; 
+                arr[index] = element;
+            }
+            Count++;
+        }
+        
+
+        public T Peek()
+        {
+            return arr[index];
+        }
+        
+
+        public T Pop()
+        {
+          
+            T element = arr[index];
+             arr[index] = default(T); 
+            --Count;
+            return element;
+        }
+    }
+
+}
---
+++ stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> myStack = new MyStack<int>(3);
+
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+
+            Console.WriteLine("PEEK : "+myStack.Peek());
+            Console.WriteLine("POP : "+myStack.Pop());
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
